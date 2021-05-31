584d2ec521924214c70eb2578c145b2237400265
Rahul255 <rahulmohan255@gmail.com>
2021-05-31 16:37:35 +0530

    file added

---
+++ GridViewApp/app/src/main/java/com/example/gridviewapp/Adapter.java
@@ -0,0 +1,45 @@
+package com.example.gridviewapp;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder> {
+
+    List<String> titles;
+    List<Integer> images;
+    Context context;
+
+    public Adapter (Context ctx,List<String> titles, List<Integer> images){
+        this.titles = titles;
+        this.images = images;
+        
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull  Adapter.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+}
