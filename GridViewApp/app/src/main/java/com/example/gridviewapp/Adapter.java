4046dd5ca3e316ca3a0a77e4dcb236454ff16a14
Rahul255 <rahulmohan255@gmail.com>
2021-06-01 10:19:18 +0530

    file added

--- GridViewApp/app/src/main/java/com/example/gridviewapp/Adapter.java
+++ GridViewApp/app/src/main/java/com/example/gridviewapp/Adapter.java
@@ -1,8 +1,11 @@
 package com.example.gridviewapp;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -13,33 +16,41 @@ public class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder> {
 
     List<String> titles;
     List<Integer> images;
-    Context context;
+    LayoutInflater inflater;
 
     public Adapter (Context ctx,List<String> titles, List<Integer> images){
         this.titles = titles;
         this.images = images;
-        
+        this.inflater = LayoutInflater.from(ctx);
+
     }
 
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View view = inflater.inflate(R.layout.custom_grid_layout,parent,false);
+        return new ViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull  Adapter.ViewHolder holder, int position) {
-
+        holder.title.setText(titles.get(position));
+        holder.gridIcon.setImageResource(images.get(position));
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return titles.size();
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
+        TextView title;
+        ImageView gridIcon;
+
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
+            title = itemView.findViewById(R.id.textView2);
+            images = itemView.findViewById(R.id.imageView2);
         }
     }
 }
--- GridViewApp/app/src/main/java/com/example/gridviewapp/MainActivity.java
+++ GridViewApp/app/src/main/java/com/example/gridviewapp/MainActivity.java
@@ -5,8 +5,14 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
     RecyclerView dataList;
+    List<String> titles;
+    List<Integer> images;
+    Adapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,5 +20,22 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         dataList = findViewById(R.id.dataList);
+
+        titles = new ArrayList<>();
+        images = new ArrayList<Integer>();
+
+        titles.add("First Item");
+        titles.add("Second Item");
+        titles.add("Third Item");
+        titles.add("Fourth Item");
+
+        images.add(R.drawable.ic_baseline_airline_seat_flat_angled_24);
+        images.add(R.drawable.ic_baseline_airplanemode_active_24);
+        images.add(R.drawable.ic_baseline_brightness_6_24);
+        images.add(R.drawable.ic_baseline_build_24);
+
+        adapter = new Adapter(this,titles,images);
+        
+
     }
 }
